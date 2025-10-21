6e76b4d82ee1092790b7a14ebd6451bb2962489a
Monil Karania <monilkarania@gmail.com>
2025-10-21 21:15:01 +0530

    mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ function App() {
         <Logo showText={true} />
       </div>
       
-      {/* Center the login/signup forms */}
+      {/* Center the login/signup forms inside a responsive container */}
       <div className="flex min-h-screen flex-col items-center justify-center p-6">
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-        </Routes>
+        <div className="w-full max-w-3xl px-4">
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   )
